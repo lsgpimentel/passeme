f3b61a3660093da0c3c666ed61804eb239598697
Léo <lsgpimentel@gmail.com>
2014-11-17 01:47:33 -0200

    Inserting name parameter in account creation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,10 +11,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # TODO Customize the permitted parameters in the future
-    #    devise_parameter_sanitizer.for(:sign_up) do |u|
-    #      u.permit(:name, :heard_how,
-    #               :email, :password, :password_confirmation)
-    #    end
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:name, :email, :password, :password_confirmation)
+    end
     #    devise_parameter_sanitizer.for(:account_update) do |u|
     #      u.permit(:name,
     #               :email, :password, :password_confirmation, :current_password)
