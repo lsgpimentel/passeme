6657b69e5d69c429cd4b172c969148f131381915
Léo <lsgpimentel@gmail.com>
2015-01-25 22:31:15 -0200

    Subnavs for the feedback menu; Putting beta sup in the name of app.

--- config/navigation.rb
+++ config/navigation.rb
@@ -62,7 +62,10 @@ SimpleNavigation::Configuration.run do |navigation|
     primary.item :reports, t('navigation.items.reports'), reports_path, icon: 'icon-bar-chart'
     #primary.item :groups, t('navigation.items.groups'), groups_path, icon: 'icon-group'
     primary.item :settings, t('navigation.items.settings'), settings_path, icon: 'icon-cogs'
-    primary.item :feedback, t('navigation.items.feedback'), 'https://docs.google.com/forms/d/11CZZugqzOc3CYY_cATA2_MuBQJN62J6n1PIh41iOYbo/viewform', icon: 'icon-thumbs-up', link: { target: '_blank' }
+    primary.item :feedback, t('navigation.items.feedback.title'),  icon: 'icon-thumbs-up' do |sub_nav|
+      sub_nav.item :evaluation_form, t('navigation.items.feedback.evaluation_form'), 'https://docs.google.com/forms/d/11CZZugqzOc3CYY_cATA2_MuBQJN62J6n1PIh41iOYbo/viewform', icon: 'icon-check', link: { target: '_blank' }
+      sub_nav.item :speak_with_author, t('navigation.items.feedback.speak_with_author'), 'mailto:lsgpimentel@gmail.com', icon: 'icon-comments', link: { target: '_blank' }
+    end
 
     # you can also specify a css id or class to attach to this particular level
     # works for all levels of the menu
