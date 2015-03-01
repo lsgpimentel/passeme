373d20535b6be4527e845c54e711433d1f60338d
Léo <lsgpimentel@gmail.com>
2015-03-01 19:41:04 -0300

    Asking for app evaluation after 2 weeks of the first timetable creation

--- lib/clock.rb
+++ lib/clock.rb
@@ -6,6 +6,7 @@ include Clockwork
 
 #Running at the day start
 every(1.day, 'Queueing Task Before Overdue Notifier', :at => '00:01') { Delayed::Job.enqueue TaskBeforeOverdueNotifier.new }
+every(1.day, 'Queueing Ask For Evaluation Notifier', :at => '01:01') { Delayed::Job.enqueue AskForEvaluationNotifier.new }
 
 #Running near the end of the day
 every(1.day, 'Queueing Follow Up Next Day Notifier', :at => '20:00') { Delayed::Job.enqueue FollowUpNextDayNotifier.new }
